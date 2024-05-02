e2fd046c87188cdd9fd5583002a37f51e8c11b85
Mashopy <eliasgheeraert@gmail.com>
2024-05-03 00:28:21 +0200

    fixup! munch: Add UClamp support for kona
    Forgot to add task_profiles.json from lahaina device tree
    
    Change-Id: Ib3571027e9678800e34b5241c5d14c89c1c03dd9

--- device.mk
+++ device.mk
@@ -300,7 +300,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/perf/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Platform
 TARGET_BOARD_PLATFORM := kona
